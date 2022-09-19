0423eed21e7d5d13d8f59b9592847c2249b7ddfe
A4nold <chinonsoekechi@gmail.com>
2022-09-19 21:35:44 +0100

    created identity server db and tables, seeded tables with values

---
+++ Mango.Services.Identity/Initializer/DbInitializer.cs
@@ -0,0 +1,81 @@
+﻿using IdentityModel;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Mango.Services.Identity.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer()
+        {
+        }
+
+        public DbInitializer(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager; 
+        }
+
+        public void Initialize()
+        {
+            if (_roleManager.FindByNameAsync(SD.Admin).Result == null)
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Customer)).GetAwaiter().GetResult();
+            }
+            else
+            {
+                return;
+            }
+
+            ApplicationUser adminUser = new ApplicationUser
+            {
+                UserName = "admin@mail.com",
+                Email = "admin@mail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "111111111",
+                FirstName = "Arnold",
+                LastName = "Admin"
+            };
+
+            _userManager.CreateAsync(adminUser, "Admin123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(adminUser, SD.Admin).GetAwaiter().GetResult();
+
+            var tmp1 = _userManager.AddClaimsAsync(adminUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, adminUser.FirstName+" "+adminUser.LastName),
+                new Claim(JwtClaimTypes.GivenName, adminUser.LastName),
+                new Claim(JwtClaimTypes.FamilyName, adminUser.FirstName),
+                new Claim(JwtClaimTypes.Role, SD.Admin)
+            }).Result;
+
+            ApplicationUser customerUser = new ApplicationUser
+            {
+                UserName = "customer@mail.com",
+                Email = "customer@mail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "111111111",
+                FirstName = "Arnold",
+                LastName = "Cust"
+            };
+
+            _userManager.CreateAsync(customerUser, "Customer123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(customerUser, SD.Customer).GetAwaiter().GetResult();
+
+            var tmp2 = _userManager.AddClaimsAsync(customerUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, customerUser.FirstName+" "+customerUser.LastName),
+                new Claim(JwtClaimTypes.GivenName, customerUser.LastName),
+                new Claim(JwtClaimTypes.FamilyName, customerUser.FirstName),
+                new Claim(JwtClaimTypes.Role, SD.Customer)
+            }).Result;
+        }
+    }
+}
--- Mango.Services.Identity/Program.cs
+++ Mango.Services.Identity/Program.cs
@@ -1,9 +1,41 @@
+using Mango.Services.Identity;
+using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.Initializer;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+var builder1 = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(SD.IdentityResources)
+.AddInMemoryApiScopes(SD.ApiScopes)
+.AddInMemoryClients(SD.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builder1.AddDeveloperSigningCredential();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 var app = builder.Build();
+
+SeedDatabase();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,6 +48,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+app.UseIdentityServer();
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -25,3 +60,12 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
