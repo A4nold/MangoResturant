3c4b7b0da284ba58f473da6e1e19e3f08702a831
A4nold <chinonsoekechi@gmail.com>
2022-09-23 19:17:57 +0100

    Shopping cart models setup and creating repository to communicate with db

---
+++ Mango.Services.ShoppingCartAPI/Migrations/20220923181654__AddedFieldToCartDetailsModel.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.ShoppingCartAPI.Migrations
+{
+    public partial class _AddedFieldToCartDetailsModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartDetails_Products_ProductId",
+                table: "CartDetails");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartDetails_Products_ProductId",
+                table: "CartDetails",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartDetails_Products_ProductId",
+                table: "CartDetails");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductId",
+                table: "CartDetails",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartDetails_Products_ProductId",
+                table: "CartDetails",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId");
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            // check if product is in db
+            //Cart cart = _mapper.Map<Cart>(cartDto);
+
+            //var prodInDb = _db.Products.FirstOrDefault(p => p.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
